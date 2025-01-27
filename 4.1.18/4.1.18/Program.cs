b916deb9bde6b66b932c2395f916c76c76c97f92
Chernihaa <Chernihaa@mail.ru>
2025-01-27 22:34:10 +0900

    !

---
+++ 4.1.18/4.1.18/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4._1._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+			var color = Console.ReadLine();
+
+			switch (color)
+			{
+				case "red":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Вы выбрали красный цвет");
+					Console.ReadLine();
+					break;
+
+				case "green":
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Вы выбрали зеленый цвет");
+					Console.ReadLine();
+					break;
+
+				case "cyan":
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Вы выбрали голубой цвет");
+					Console.ReadLine();
+					break;
+
+				default:
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine("Вы выбрали цвет по default - это желтый");
+					Console.ReadLine();
+					break;
+			}
+		}
+    }
+}
